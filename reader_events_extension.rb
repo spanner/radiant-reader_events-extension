a90794f0f7b5032ce8805e1c194cb37dfdbb2e14
spanner <will@spanner.org>
2010-04-14 08:24:29 +0100

    configuration

--- reader_events_extension.rb
+++ reader_events_extension.rb
@@ -3,7 +3,7 @@
 
 class ReaderEventsExtension < Radiant::Extension
   version "1.0"
-  description "Describe your extension here"
+  description "Knits together reader and event_calendar extensions to allow readers to submit events to a holding pen"
   url "http://yourwebsite.com/reader_events"
   
   # define_routes do |map|
@@ -12,8 +12,13 @@ class ReaderEventsExtension < Radiant::Extension
   #   end
   # end
   
+  extension_config do |config|
+    config.extension 'reader'
+    config.extension 'event_calendar'
+  end
+  
   def activate
-    # admin.tabs.add "Reader Events", "/admin/reader_events", :after => "Layouts", :visibility => [:all]
+
   end
   
   def deactivate
